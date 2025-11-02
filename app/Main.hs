62a330d6baf0878a5c6328f28dafdbfdab4d1704
Asafe Klain <asafeklain77@gmail.com>
2025-11-02 17:55:35 +0000

    main deal with arguments

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,26 @@ module Main where
 import Lambda
 import Prelude hiding (lex)
 import System.IO
+import System.Environment (getArgs)
 
 main :: IO ()
 main = do
+  args <- getArgs
+  if args == [] then 
+    runRepl 
+  else 
+    runFile (head args)
+
+runFile :: FilePath -> IO ()
+runFile f = do
+  content <- readFile f
+  putStrLn (show $ eval (parse (lex content)))
+
+runRepl :: IO ()
+runRepl = do
   hSetBuffering stdout NoBuffering
   putStrLn "Welcome to Lambda repl"
   putStr "lambda> "
   interact (concat . fmap repl . lines)
   where
-  repl str = (show $ eval (parse (lex str))) <> "\nlambda> "
+    repl str = (show $ eval (parse (lex str))) <> "\nlambda> "
